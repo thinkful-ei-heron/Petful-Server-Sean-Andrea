2db2ef165e9cafdab21668faa7553ca522ef440f
Andrea Bender <andreab.1726@gmail.com>
2019-12-20 09:35:18 -0700

    added queue class

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
+/* eslint-disable indent */
+'use strict';
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-}
+	PORT: process.env.PORT || 8000,
+	NODE_ENV: process.env.NODE_ENV || 'development'
+};
+
+//module.exports = {
+// CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+// PORT : process.env.PORT || 8080
+// }
